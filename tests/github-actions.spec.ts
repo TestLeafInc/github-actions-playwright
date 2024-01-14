597ac8eb527b79177cec614ce3aedbba7fdb7af8
Babu Manickam <babu@testleaf.com>
2024-01-14 18:31:10 +0530

    "Baseline Code for PW - GitHub Actions"

---
+++ tests/github-actions.spec.ts
@@ -0,0 +1,15 @@
+import test, { expect } from "@playwright/test";
+
+test.describe(`Salesforce login - GitHub Actions`,async()=>{
+
+    test(`Login to salesforce`,async({page})=>{
+        
+        await page.goto("https://login.salesforce.com/")
+        await page.locator("#username").fill("ranjini.r@testleaf.com")
+        await page.locator("#password").fill("Testleaf$1234")
+        await page.locator("#Login").click()
+        
+        const appLauncherLocator = page.locator(".slds-icon-waffle")
+
+    })
+})
